6105bb2c491346028997ff78a32071bee593acac
abhi1506manu <abhi2019manu@gmail.com>
2023-07-05 11:52:24 +0530

    designed sidebar component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import App from "./App";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <ThirdwebProvider desiredChainId = {ChainId.Goerli}>
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <Router>
       <App />
     </Router>
