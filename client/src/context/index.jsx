263154a6b7de30286d3e1632b2e2822230d01112
abhi1506manu <abhi2019manu@gmail.com>
2023-07-08 13:04:16 +0530

    fetching campaigns

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -40,6 +40,24 @@ export const StateContextProvider = ({ children }) => {
       console.log("Contract called failure", error);
     }
   };
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCampaigns");
+    // console.log(campaigns)
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+    return parsedCampaigns;
+  };
   return (
     <StateContext.Provider
       value={{
@@ -47,6 +65,7 @@ export const StateContextProvider = ({ children }) => {
         contract,
         connect,
         createCampaign: publishCampaign,
+        getCampaigns,
       }}
     >
       {children}
